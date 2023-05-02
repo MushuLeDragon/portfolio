67557755fb3c82761ad423e00831345b48c7ab50
MushuLeDragon <dragon.mushu@hotmail.com>
2023-05-02 10:36:55 +0200

    add baseUrl to config to fix inks project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astronaut.github.io',
-  base: '/portfolio'
+  base: '/portfolio',
+  server: {
+    baseUrl: '/portfolio/',
+  },
 });
